38ccc442dc984a155320d4d8297bcf01d0919439
Swompen <Swompen@gmail.com>
2022-10-26 05:54:26 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,9 +2,9 @@ Config = {
     Lan = "en",
     MinPrice = 800,
     MaxPrice = 1400,
-    RenewedPhone = true,
+    RenewedPhone = false,
     MechanicJobName = "mechanic",
-	RenewedBanking = true, -- Enable if using Renewed-Banking (For transactions history)
+    RenewedBanking = false, -- Enable if using Renewed-Banking (For transactions history)
     GiveMechanicJobMoney = true, -- If money should go to mechanic job (Only works with RenewedBanking)
     PayProcent = 0.6, -- 0.6 = 60% (Only works with RenewedBanking)
 }
